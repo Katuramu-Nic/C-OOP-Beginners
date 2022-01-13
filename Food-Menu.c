a1a886f5013b2d746a91c8f5fd5b1b1a8b8b5301
Katuramu-Nic <katuramu.ict@gmail.com>
2022-01-13 18:17:40 +0300

    another Tututorial
    A good practice

---
+++ Food-Menu.c
@@ -0,0 +1,19 @@
+/*include<iostream>
+include<string>
+main int()
+{
+ cout<<"Hello World";
+ return 0;
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
