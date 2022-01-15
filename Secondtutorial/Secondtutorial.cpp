29222aeccc0288dc971034c83d3b62e7339bb76d
Katuramu-Nic <katuramu.ict@gmail.com>
2022-01-15 12:34:36 +0300

    Latest Update
     OOP Programming

--- Secondtutorial/Secondtutorial.cpp
+++ Secondtutorial/Secondtutorial.cpp
@@ -1,13 +1,43 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
 class DayMeal{
-      public:
-string Mfood;
+//Getters and setters, encapsulated data
+    private:
+    string Mfood;
 int Mquantity;
 int Munitprice;
 string Mcustomer;
 int Mtotal;
+      public:
+          void setMfood(string mfood)
+          {
+              Mfood = mfood;
+          }
+          string getMfood()
+          {
+              return Mfood;
+          }
+
+          void setMquantity(int mquantity)
+          {
+              Mquantity = mquantity;
+          }
+          int getMquantity()
+          {
+              return Mquantity;
+          }
+
+          void setMunitprice(int munitprice)
+          {
+              Munitprice = munitprice;
+          }
+          int getMunitprice()
+          {
+              return Munitprice;
+          }
+//Output methods
 void AskforAmeal()
 {
     cout<<"Customer Name: "<<Mcustomer<<endl;
@@ -15,11 +45,12 @@ void AskforAmeal()
     cout<<"Unit Price: "<<Munitprice<<endl;
     cout<<"Quantity: "<<Mquantity<<endl;
     Mtotal = Mquantity * Munitprice;
-    cout<<"Total Amount: Kshs."<<Mtotal;
+    cout<<"Total Amount: Kshs."<<Mtotal<<endl;
 }
 /*The Day Meal Constructor method*/
 DayMeal(string mfood, int mquantity, int munitprice, string mcustomer)
 {
+    //Initializing variables in the Meals constructor method
     Mfood = mfood;
     Mquantity = mquantity;
     Munitprice = munitprice;
@@ -29,17 +60,18 @@ DayMeal(string mfood, int mquantity, int munitprice, string mcustomer)
 };
 int main()
 {
+    //First Object
 DayMeal firstdaymeal = DayMeal("Rice & Beef",2,400,"Katuramu Nicholus");
 firstdaymeal.AskforAmeal();
-
-
+//Second Object
 DayMeal seconddaymeal = DayMeal("Ugali & Beef",4,520,"Kipcaren Marvin");
 seconddaymeal.AskforAmeal();
-
+//Third Object
 DayMeal thirddaymeal = DayMeal("Sweet Potatoes & Beef",3,400,"Denis Mungata");
 thirddaymeal.AskforAmeal();
-
+//Fourth Object
 DayMeal fourthdaymeal = DayMeal("Yams & Beef",6,200,"Peter Waweru");
 fourthdaymeal.AskforAmeal();
-
+firstdaymeal.setMunitprice(40);
+cout<<"The First Order Meal Price has changed to "<<firstdaymeal.getMunitprice()<<"Kshs.";
 }
