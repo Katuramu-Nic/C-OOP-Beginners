202f667751c6178e5500cd60ee77d56f2665fe2c
Katuramu-Nic <katuramu.ict@gmail.com>
2022-01-25 17:34:55 +0300

    The C++ Login System
    Pushing on with c++

--- Secondtutorial/loginsystem.cpp
+++ Secondtutorial/loginsystem.cpp
@@ -1,5 +1,32 @@
 #include<iostream>
 #include<string>
+using namespace std;
+class LoginSystem{
+string Username;
+string Password;
+string FirstName;
+string LastName;
+string Emailaddress;
+
+void Signup()
+{
+Username = "Katuramu";
+Password = "Jean@123";
+    cout<<"Username:"<<endl;
+    cin>>Username;
+    cout<<"Password:"<<endl;
+    cin>>Password;
+    cout<<"First Name: "<<endl;
+    cin>>FirstName;
+    cout<<"Last Name: "<<endl;
+    cin>>LastName;
+    cout<<"Email Address: "<<endl;
+    if (Username=="Katuramu" && Password=="Jean@123")
+        cout<<"You are LoggedIn";
+    else
+    cout<<"Your Username or Password is Wrong"<<endl;
+}
+};
 int main()
 {
 
